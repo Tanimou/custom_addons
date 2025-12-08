310dc6ab81a80a04d00e74c4f9ef6c1915657f31
Cisse tanimou <tanimou@live.fr>
2025-12-08 13:21:14 +0000

    refactor: Improve import statements for better readability in shipment tracking models and wizards

--- custom_shipment_tracking/wizard/__init__.py
+++ custom_shipment_tracking/wizard/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import parcel_product_wizard
-from . import shipment_regrouping_wizard
+from . import parcel_product_wizard, shipment_regrouping_wizard
