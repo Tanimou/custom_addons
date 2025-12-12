8a875dfb3fe6ccfb288c258c98dd692cd6194637
Cisse tanimou <tanimou@live.fr>
2025-12-12 13:56:34 +0000

    feat: Add custom purchases module with manifest, models, and icons

---
+++ migrations/custom_purchases/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Personnalisation des Achats',
+    'version': '1.1.0',
+    'summary': 'Ce module permet de personnaliser les achats',
+    'description': 'ce module permet de personnaliser les achats',
+    'sequence': -100,
+    'category': 'Purchases/invantory',
+    'author': 'Partenaire de succès',
+    'website': 'https://www.partenairesucces.com/',
+    'depends': [
+        'base',
+        'purchase',
+        'product',
+        'sale',
+    ],
+    'data': [
+
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False
+}
